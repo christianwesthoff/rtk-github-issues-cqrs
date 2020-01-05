da7cf6280d0b975f2a36e0fba4460fdcf0e2fbd9
Christian Westhoff <westhoffchristian1@gmail.com>
2020-01-05 18:15:42 +0100

    WIP

--- src/extensions/extensions.ts
+++ src/extensions/extensions.ts
@@ -9,7 +9,6 @@ import {
  } from '@reduxjs/toolkit'
 import { ThunkAction } from 'redux-thunk'
 
-// TODO: distinct between effect and normal reducers
 export interface QueryOptions<
   State,
   CR extends SliceCaseReducers<State> = SliceCaseReducers<State>,
